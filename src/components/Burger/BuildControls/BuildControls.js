94307a3a957ef84a9560c955fc77461c7b2af8b9
davise-networks <davise.networks@gmail.com>
2019-08-18 21:33:24 +0530

    Initial Commit

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+
+import classes from './BuildControls.css';
+
+const buildControls = (props) => {
+    return (
+        <div>
+            <div></div>
+        </div>
+    )
+};
+
+export default buildControls;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Aux from '../../hoc/Auxi';
+
+import classes from './Layout.css';
+
+const layout = (props) => {
+    return (
+        <Aux>
+            <div>
+                Toolbar, Sidedrawer, Backdrop
+            </div>
+            <main className={classes.content}>
+                {props.children}
+            </main>
+        </Aux>
+    )
+};
+
+export default layout;
